c4608cf3acc1cebc6f0b16364d71c398e2eb20f9
Taehee Kim <taehee@nalbi.ai>
2023-03-21 17:22:46 +0900

    fix: https 접두어 추가

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const state = params.get('state')
 console.log(auth0domain, state)
 
 const $form = document.getElementById('form-agree')
-if (auth0domain !== null && state !== null) {
-    $form.action = encodeURI(`${auth0domain}/continue?state=${state}`);
+if (auth0domain !== null) {
+    $form.action = `https://${auth0domain}/continue?state=${state ?? ''}`;
 } else {
     alert("잘못된 접근입니다.")
     history.back();
