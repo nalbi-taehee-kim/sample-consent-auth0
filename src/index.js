484eb3add752f8aca6795e8b746dd9c765fe1bb9
Taehee Kim <taehee@nalbi.ai>
2023-03-21 17:14:04 +0900

    feat: redirect to 추가

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+
+const params = new URLSearchParams(window.location.search)
+const auth0domain = params.get('auth0_domain')
+const state = params.get('state')
+console.log(auth0domain, state)
+
+const $form = document.getElementById('form-agree')
+if (auth0domain !== null && state !== null) {
+    $form.action = encodeURI(`${auth0domain}/continue?state=${state}`);
+} else {
+    alert("잘못된 접근입니다.")
+    history.back();
+}
